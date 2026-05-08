d78dcd0f49f74edb3f2094c026404c7cb05365f2
Peter <drpetdone@gmail.com>
2026-05-08 10:34:32 +0200

    meaningful updates

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session  
+
+from database import SessionLocal, engine, Base
+import models
+import schemas
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Dependency to get DB session 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 
 @app.get("/")
 def home():
